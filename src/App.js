2015e31726657d5cbc2980a90362a1fcbf1fedbb
dhruvisha <dhruvishasatani123@gmail.com>
2024-09-17 06:58:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './Components/Login';
+// import{Routes ,Route} from 'react-router-dom/cjs/react-router-dom'
+import TaskList from './Task/TaskList';
+import Registration from './Components/Registration'
+import { useState } from 'react';
+
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [currentUser, setCurrentUser] = useState(null);
+
+  const registerUser = (username, password) => {
+    const userExists = users.some(user => user.username === username);
+    if (userExists) {
+      alert("Username already exists!");
+    } else {
+      setUsers([...users, { username, password }]);
+      alert("Registration successful!");
+    }
+  };
+
+  const loginUser = (username, password) => {
+    const user = users.find(user => user.username === username && user.password === password);
+    if (user) {
+      setCurrentUser(user);
+    } else {
+      alert("Invalid username or password");
+    }
+  };
+
+  const logoutUser = () => {
+    setCurrentUser(null);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='form'>
+
+      {/* <Routes>
+          <Route path={'/login'} exact Components={Login}> </Route>
+          <Route path={'/Registration'} exact Components={Login}> </Route>
+      </Routes>
+     
+          <Login/> */}
+
+
+
+      {!currentUser ? (
+        <>
+        
+          <Registration registerUser={registerUser} />
+          <Login loginUser={loginUser} />
+        </>
+      ) : (
+        <TaskList currentUser={currentUser} logoutUser={logoutUser} />
+      )}
+      
+     
+     
     </div>
   );
 }
